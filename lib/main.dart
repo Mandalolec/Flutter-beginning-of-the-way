cbb6c77013c3fcd04540d57d9e6078275dfacfa0
Mandalolec <kirillwa777@gmail.com>
2022-05-08 16:41:51 +0700

    Testing different widgets
    Testing Container, Expanded, Column

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,59 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Align(
-      alignment: Alignment.center,
-      child: Text(
-        "Привет Flutter!",
-        textDirection: TextDirection.ltr,
-        textAlign: TextAlign.center,
-      ),
-    ),
+    Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+                color: Colors.tealAccent,
+                alignment: Alignment.center,
+                child: const Text(
+                    "Hello, Flutter",
+                    style: TextStyle(color: Colors.black,
+                      fontSize: 40,
+                    ),
+                    textDirection: TextDirection.ltr
+                )
+            ),
+            flex: 1,
+          ),
+          Expanded(
+              child: Container(
+                  color: Colors.amber,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container (
+                        alignment: Alignment.center,
+                        child: const Text(
+                            "Hello, Flutter again",
+                            style: TextStyle(color: Colors.deepPurple,
+                              fontSize: 20,
+                            ),
+                            textDirection: TextDirection.ltr
+                        ),
+                      ),
+                      Container (
+                        alignment: Alignment.center,
+                        child: const Text(
+                            "Yo, so many strings!",
+                            style: TextStyle(color: Colors.white,
+                              fontSize: 20,
+                            ),
+                            textDirection: TextDirection.ltr
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+                  flex: 6
+          )
+
+        ]
+
+    )
 
   );
 }
