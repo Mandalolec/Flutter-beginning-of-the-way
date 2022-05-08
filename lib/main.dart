a7b776796bcff9f120fa7814ffc8a41f679ae695
Mandalolec <kirillwa777@gmail.com>
2022-05-08 17:35:37 +0700

    Deleted widget_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Column(
+  runApp(Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Expanded(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              color: Colors.tealAccent,
+              alignment: Alignment.center,
+              child: const Text("Hello, Flutter",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                  ),
+                  textDirection: TextDirection.ltr)),
+          flex: 1,
+        ),
+        Expanded(
             child: Container(
-                color: Colors.tealAccent,
+                color: Colors.amber,
                 alignment: Alignment.center,
-                child: const Text(
-                    "Hello, Flutter",
-                    style: TextStyle(color: Colors.black,
-                      fontSize: 40,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.center,
+                      child: const Text("Hello, Flutter again",
+                          style: TextStyle(
+                            color: Colors.deepPurple,
+                            fontSize: 20,
+                          ),
+                          textDirection: TextDirection.ltr),
                     ),
-                    textDirection: TextDirection.ltr
-                )
-            ),
-            flex: 1,
-          ),
-          Expanded(
-              child: Container(
-                  color: Colors.amber,
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Container (
-                        alignment: Alignment.center,
-                        child: const Text(
-                            "Hello, Flutter again",
-                            style: TextStyle(color: Colors.deepPurple,
-                              fontSize: 20,
-                            ),
-                            textDirection: TextDirection.ltr
-                        ),
-                      ),
-                      Container (
-                        alignment: Alignment.center,
-                        child: const Text(
-                            "Yo, so many strings!",
-                            style: TextStyle(color: Colors.white,
-                              fontSize: 20,
-                            ),
-                            textDirection: TextDirection.ltr
-                        ),
-                      ),
-                    ],
-                  )
-              ),
-                  flex: 6
-          )
-
-        ]
-
-    )
-
-  );
+                    Container(
+                      alignment: Alignment.center,
+                      child: const Text("Yo, so many strings!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          textDirection: TextDirection.ltr),
+                    ),
+                  ],
+                )),
+            flex: 6)
+      ]));
 }
